7cf8f3ae615d2d594b5239ca4fc681f76ac44d2e
Joka <ivan.joskic91@gmail.com>
2023-12-07 17:57:10 +0100

    small changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -17,7 +17,7 @@ const Navbar = () => {
     })
   }, [])
 
-  function handleAuthentication () {
+  function handleAuthentication() {
     if (user.loggedIn) {
       fcl.unauthenticate()
     } else {
@@ -26,29 +26,30 @@ const Navbar = () => {
   }
 
   return (
-        <nav className='navbar'>
-            <div className='navbar-left'>
-                <Link href='/'>Home</Link>
-                <Link href='/mint'>Mint</Link>
-                <Link href='/collected'>Collected</Link>
-            </div>
-            <div className='navbar-right'>
-                    {
-                    user.loggedIn 
-                    ?
+    <nav className='navbar'>
+      <div className='navbar-left'>
+        <Link href='/'>Home</Link>
+        <Link href='/mint'>Mint</Link>
+        <Link href='/collected'>Collected</Link>
+      </div>
+      <div className='navbar-right'>
+        {
+          user.loggedIn
+            ?
+            (
 
-                        (
-
-                    <>
-                        <p>{user.addr}</p>
-                        <button onClick={handleAuthentication}>Log Out</button>
-                    </>
-                        ) : (
-
-                    <button onClick={handleAuthentication}>Log In</button>
-                        )}
-            </div>
-        </nav>
+              <>
+                <p>{user.addr}</p>
+                <button onClick={handleAuthentication}>Log Out</button>
+              </>
+            )
+            :
+            (
+              <button onClick={handleAuthentication}>Log In</button>
+            )
+        }
+      </div>
+    </nav>
   )
 }
 
